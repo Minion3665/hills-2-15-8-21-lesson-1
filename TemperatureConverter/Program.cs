93d9aab8010bc24a11b180b4f30820114a2a0f28
Skyler Turner <skyler@clicksminuteper.net>
2021-09-15 12:25:22 +0100

    Initial commit, add temperature conversion program

---
+++ TemperatureConverter/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TemperatureConverter
+{
+    // ReSharper disable once ClassNeverInstantiated.Global
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Enter a temperature (Fahrenheit): ");
+            var success = float.TryParse(Console.ReadLine(), out var fahrenheit);
+            Console.WriteLine(success
+                ? $"Your temperature in Celsius is {Math.Round(0.555 * (fahrenheit - 32), 2)}"
+                : "You need to enter a valid temperature");
+        }
+    }
+}
